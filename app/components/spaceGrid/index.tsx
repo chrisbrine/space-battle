0aa3f32a1794f4593908a809aa23d9fb04391c62
Chris Brine <cbrine@gala.games>
2024-02-07 23:37:44 -0800

    Fix

--- app/components/spaceGrid/index.tsx
+++ app/components/spaceGrid/index.tsx
@@ -1,12 +1,17 @@
 'use client';
 
 import { useEffect } from "react";
-import { SpaceGrid as Space } from "../../data/space-grid";
+import { SpaceGrid as Space } from "@/app/data/space-grid";
+
+
 
 export default function SpaceGrid() {
+  // full screen sized grid of space with stars and planets using tailwind css and canvas
+
+  // draw stars and planets in the canvas
   useEffect(() => {
-    const canvas = document.getElementById('space') as HTMLCanvasElement;
-    const ctx = canvas.getContext('2d');
+    const canvas: HTMLCanvasElement | null = document.getElementById('space') as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
     if (canvas && ctx) {
       const space = new Space(canvas, ctx, 1000);
       space.draw();
@@ -14,8 +19,8 @@ export default function SpaceGrid() {
   }, []);
 
   return (
-    <div className="bg-black overflow-hidden" style={{top: 0, left: 0, position: 'fixed', height: '100hw', width: '100vw'}}>
-      <canvas id="space" className="bg-black h-screen w-screen overflow-hidden" style={{zIndex: 10}}></canvas>
+    <div className="bg-black h-screen w-screen">
+      <canvas id="space" className="h-full w-full"></canvas>
     </div>
   );
 }
